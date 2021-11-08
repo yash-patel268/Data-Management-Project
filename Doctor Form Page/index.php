9f1ba197ef405f308d9a2df89da6289a3bfe2f4f
Yash <yashp2126@gmail.com>
2021-11-08 12:32:48 -0500

    Added doctor form page

---
+++ Doctor Form Page/index.php
@@ -0,0 +1,71 @@
+<?php
+    $email = $_GET['email'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Patient Registration Form</title>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="title">
+            Doctor Registration Form
+        </div>
+        <form action="patient.php" method="POST">
+            <div class="form">
+                <input type="hidden" name="email" value="<?php echo $email ?>" /> 
+                <div class="inputField">
+                    <label>First Name</label>
+                    <input type="text" class="input" name="firstname">
+                </div>
+
+                <div class="inputField">
+                    <label>Last Name</label>
+                    <input type="text" class="input" name="lastname">
+                </div>
+
+                <div class="inputField">
+                    <label>Phone Number</label>
+                    <input type="text" class="input" name="phonenumber">
+                </div>
+
+                <div class="inputField">
+                    <label>Age</label>
+                    <input type="text" class="input" name="age">
+                </div>
+
+                <div class="inputField">
+                    <label>Gender</label>
+                    <input type="text" class="input" name="gender">
+                </div>
+
+                <div class="inputField">
+                    <label>Date of Birth</label>
+                    <input type="text" class="input" name="dayOfBirth">
+                </div>
+
+                <div class="inputField">
+                    <label>Addresss</label>
+                    <textarea class="textarea" name="address"></textarea>
+                </div>
+
+                <div class="inputField terms">
+                    <label class="check">
+                        <input type="checkbox">
+                        <span class="checkmark"></span>
+                    </label>
+                    <p>Agree to terms and conditions</p>
+                </div> 
+
+                <div class="inputField">
+                    <input type="submit" value="Register" class="btn">
+                </div>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
---
+++ Doctor Form Page/patient.php
@@ -0,0 +1,49 @@
+<?php
+    $email = $_POST['email'];
+
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "clinicregistration";
+    $portnumber = "3308";
+
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname, $portnumber);
+
+    $sql="SELECT * FROM logintable WHERE email='$email'";
+
+    $query =  mysqli_query($conn, $sql);
+    
+    while($row=mysqli_fetch_array($query)){
+        $username = $row['username'];
+        $pass = $row['pass'];
+    }
+
+
+    $firstname = $_POST['firstname'];
+    echo "$firstname\n";
+    $lastname = $_POST['lastname'];
+    echo "$lastname\n";
+    $phonenumber = $_POST['phonenumber'];
+    echo "$phonenumber\n";
+    $age = $_POST['age'];
+    echo "$age\n";
+    $gender = $_POST['gender'];
+    echo "$gender\n";
+    $dayOfBirth = $_POST['dayOfBirth'];
+    echo "$dayOfBirth\n";
+
+    echo "$email\n";
+    echo "$username\n";
+    echo "$pass\n";
+
+    $address = $_POST['address'];
+    echo "$address\n";
+    
+    $INSERT = "INSERT INTO patientreg (firstname, lastname, phonenumber, age, gender, dayOfBirth, username, email, pass, address) values(?,?,?,?,?,?,?,?,?,?) ";
+
+    $stmt = $conn->prepare($INSERT);
+    $stmt->bind_param("ssssssssss",  $firstname,$lastname,$phonenumber,$age, $gender,$dayOfBirth,$username,$email,$pass,$address);
+    $stmt->execute();
+
+    header("location:../Patient Main Page/index.php?email=$email");
+?>
