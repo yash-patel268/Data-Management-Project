5754b37653c711a1301912849d84e1f0965fb6e1
Yash <yashp2126@gmail.com>
2021-11-09 16:55:01 -0500

    Fixed php

--- Patient Prescription Form/prescription.php
+++ Patient Prescription Form/prescription.php
@@ -9,40 +9,31 @@
 
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname, $portnumber);
 
-    $sql="SELECT * FROM logintable WHERE email='$email'";
+    $sql="SELECT * FROM patientreg WHERE email='$email'";
 
     $query =  mysqli_query($conn, $sql);
     
     while($row=mysqli_fetch_array($query)){
-        $username = $row['username'];
-        $pass = $row['pass'];
+        $patientid = $row['patientid'];
     }
 
+    $prescriptionName = $_POST['prescriptionname'];
+    $docLastName = $_POST['doclastname'];
+    $start = $_POST['start'];
+    $end = $_POST['end'];
 
-    $firstname = $_POST['firstname'];
-    echo "$firstname\n";
-    $lastname = $_POST['lastname'];
-    echo "$lastname\n";
-    $phonenumber = $_POST['phonenumber'];
-    echo "$phonenumber\n";
-    $age = $_POST['age'];
-    echo "$age\n";
-    $gender = $_POST['gender'];
-    echo "$gender\n";
-    $dayOfBirth = $_POST['dayOfBirth'];
-    echo "$dayOfBirth\n";
+    $sql="SELECT * FROM doctorreg WHERE doclastname='$docLastName'";
 
-    echo "$email\n";
-    echo "$username\n";
-    echo "$pass\n";
-
-    $address = $_POST['address'];
-    echo "$address\n";
+    $query =  mysqli_query($conn, $sql);
     
-    $INSERT = "INSERT INTO patientreg (firstname, lastname, phonenumber, age, gender, dayOfBirth, username, email, pass, address) values(?,?,?,?,?,?,?,?,?,?) ";
+    while($row=mysqli_fetch_array($query)){
+        $doctorid = $row['doctorid'];
+    }
+
+    $INSERT = "INSERT INTO prescription (patientid, doctorid, prescriptionname, start, end) values(?,?,?,?,?)";
 
     $stmt = $conn->prepare($INSERT);
-    $stmt->bind_param("ssssssssss",  $firstname,$lastname,$phonenumber,$age, $gender,$dayOfBirth,$username,$email,$pass,$address);
+    $stmt->bind_param("sssss",  $patientid,$doctorid, $prescriptionName, $start, $end);
     $stmt->execute();
 
     header("location:../Patient Main Page/index.php?email=$email");
