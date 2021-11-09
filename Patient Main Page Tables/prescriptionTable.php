6d607eaca3cfed2e208326bab863f6d92e8d25aa
Yash <yashp2126@gmail.com>
2021-11-09 17:16:03 -0500

    Fixed the connection to database

--- Patient Main Page Tables/prescriptionTable.php
+++ Patient Main Page Tables/prescriptionTable.php
@@ -8,8 +8,17 @@
 
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname, $portnumber);
 
-    $sql="SELECT * FROM doctorreg";
+    $sql="SELECT * FROM patientreg WHERE email='$email'";
+
+    $query =  mysqli_query($conn, $sql);
+    
+    while($row=mysqli_fetch_array($query)){
+        $patientid = $row['patientid'];
+    }
+
+    $sql="SELECT * FROM prescription WHERE patientid='$patientid'";
     $result = mysqli_query($conn, $sql);
+    
 
 ?>
 
@@ -20,27 +29,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>View Doctors in system</title>
+    <title>View Your Presciptions</title>
 </head>
 <body>
     <table align="center" border="1px" style="width:600px; line-height:40px;">
         <tr>
-            <th colspan="4"><h2>View Doctors</h2></th>
+            <th colspan="3"><h2>Your Prescriptions</h2></th>
         </tr>
         <t>
-            <th>Last Name</th>
-            <th>First Name</th>
-            <th>Gender</th>
-            <th>Email</th>
+            <th>Presciption Name</th>
+            <th>Start Date</th>
+            <th>End Date</th>
         </t>
         <?php
             while($rows=mysqli_fetch_assoc($result)){
         ?>
             <tr>
-                <td align="center"><?php echo $rows['doclastname']; ?></td>
-                <td align="center"><?php echo $rows['doclastname']; ?></td>
-                <td align="center"><?php echo $rows['docgender']; ?></td>
-                <td align="center"><?php echo $rows['email']; ?></td>
+                <td align="center"><?php echo $rows['prescriptionname']; ?></td>
+                <td align="center"><?php echo $rows['start']; ?></td>
+                <td align="center"><?php echo $rows['end']; ?></td>
             </tr>
         <?php
             }
