56032da5ed3c1c9d553d1d48b77e2bf9349599b7
Yash <yashp2126@gmail.com>
2021-11-10 21:19:05 -0500

    Fixed connection to database

--- Doctor Main Page Tables/appointmentTableAll.php
+++ Doctor Main Page Tables/appointmentTableAll.php
@@ -8,15 +8,7 @@
 
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname, $portnumber);
 
-    $sql="SELECT * FROM patientreg WHERE email='$email'";
-
-    $query =  mysqli_query($conn, $sql);
-    
-    while($row=mysqli_fetch_array($query)){
-        $patientid = $row['patientid'];
-    }
-
-    $sql="SELECT * FROM appointments WHERE patientid='$patientid'";
+    $sql="SELECT * FROM appointments";
     $result = mysqli_query($conn, $sql);
     
 
@@ -29,21 +21,34 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>View Your Appointments</title>
+    <title>View All Appointments</title>
 </head>
 <body>
     <table align="center" border="1px" style="width:600px; line-height:40px;">
         <tr>
-            <th colspan="2"><h2>Your Appointments</h2></th>
+            <th colspan="4"><h2>All Appointments</h2></th>
         </tr>
         <t>
+            <th>First Name</th>
+            <th>Last Name</th>
             <th>Chosen Date of Appointment</th>
             <th>Time of Appointment</th>
         </t>
         <?php
             while($rows=mysqli_fetch_assoc($result)){
+                $patientid = $rows['patientid'];
+                $sql="SELECT * FROM patientreg WHERE patientid='$patientid'";
+
+                $query =  mysqli_query($conn, $sql);
+    
+                while($row=mysqli_fetch_array($query)){
+                    $firstname = $row['firstname'];
+                    $lastname = $row['lastname'];
+                }
         ?>
             <tr>
+                <td align="center"><?php echo $firstname; ?></td>
+                <td align="center"><?php echo $lastname; ?></td>
                 <td align="center"><?php echo $rows['choosendate']; ?></td>
                 <td align="center"><?php echo $rows['time']; ?></td>
             </tr>
