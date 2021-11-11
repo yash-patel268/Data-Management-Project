ebc5b44bfc6a0b4846be7a2b434cb5f13370e66d
Yash <yashp2126@gmail.com>
2021-11-10 21:04:28 -0500

    Fixed connection to database

--- Doctor Main Page Tables/prescriptionTableAll.php
+++ Doctor Main Page Tables/prescriptionTableAll.php
@@ -8,15 +8,7 @@
 
     $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname, $portnumber);
 
-    $sql="SELECT * FROM patientreg WHERE email='$email'";
-
-    $query =  mysqli_query($conn, $sql);
-    
-    while($row=mysqli_fetch_array($query)){
-        $patientid = $row['patientid'];
-    }
-
-    $sql="SELECT * FROM prescription WHERE patientid='$patientid'";
+    $sql="SELECT * FROM prescription";
     $result = mysqli_query($conn, $sql);
     
 
@@ -29,22 +21,35 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title>View Your Presciptions</title>
+    <title>View All Presciptions</title>
 </head>
 <body>
     <table align="center" border="1px" style="width:600px; line-height:40px;">
         <tr>
-            <th colspan="3"><h2>Your Prescriptions</h2></th>
+            <th colspan="5"><h2>All Prescriptions</h2></th>
         </tr>
         <t>
+            <th>First Name</th>
+            <th>Last Name</th>
             <th>Presciption Name</th>
             <th>Start Date</th>
             <th>End Date</th>
         </t>
         <?php
             while($rows=mysqli_fetch_assoc($result)){
+                $patientid = $rows['patientid'];
+                $sql="SELECT * FROM patientreg WHERE patientid='$patientid'";
+
+                $query =  mysqli_query($conn, $sql);
+    
+                while($row=mysqli_fetch_array($query)){
+                    $firstname = $row['firstname'];
+                    $lastname = $row['lastname'];
+                }
         ?>
             <tr>
+                <td align="center"><?php echo $firstname; ?></td>
+                <td align="center"><?php echo $lastname; ?></td>
                 <td align="center"><?php echo $rows['prescriptionname']; ?></td>
                 <td align="center"><?php echo $rows['start']; ?></td>
                 <td align="center"><?php echo $rows['end']; ?></td>
