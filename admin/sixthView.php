140098792ab99e03b89cd5e252c805a9889560f4
Yash <yashp2126@gmail.com>
2021-11-15 12:38:22 -0500

    Fixed typo

--- admin/sixthView.php
+++ admin/sixthView.php
@@ -17,7 +17,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>1st View</title>
+    <title>6th View</title>
 </head>
 <body>
     <table align="center" border="1px" style="width:600px; line-height:40px;">
