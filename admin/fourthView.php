a20f4d74c3a798639681b2eecb2535a33f8608e6
Yash <yashp2126@gmail.com>
2021-11-15 16:37:56 -0500

    Added fourth view php

---
+++ admin/fourthView.php
@@ -0,0 +1,49 @@
+<?php
+    $host = "localhost";
+    $dbUsername = "root";
+    $dbPassword = "";
+    $dbname = "clinicregistration";
+    $portnumber = "3308";
+
+    $conn = new mysqli($host, $dbUsername, $dbPassword, $dbname, $portnumber);
+    
+    $sql = "SELECT
+            patientreg.lastname,
+            logintable.loginid
+            From patientreg
+            LEFT JOIN logintable 
+            ON patientreg.email=logintable.email
+            order by loginid";
+
+    $query =  mysqli_query($conn, $sql);
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>4th View</title>
+</head>
+<body>
+    <table align="center" border="1px" style="width:600px; line-height:40px;">
+        <tr>
+            <th colspan="2"><h2>4th View Left Join ("FULL JOIN")</h2></th>
+        </tr>
+        <t>
+            <th>Last Name</th>
+            <th>Login ID</th>
+        </t>
+        <?php
+            while($rows=mysqli_fetch_assoc($query)){
+        ?>
+            <tr>
+                <td align="center"><?php echo $rows['lastname']; ?></td>
+                <td align="center"><?php echo $rows['loginid']; ?></td>
+            </tr>
+        <?php
+            }
+        ?>
+    </table>
+</body>
+</html>
